0211fbd77f1e6516f2aa2adfe60503c7811da5c1
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-09-27 18:02:04 -0600

    refactor cypress to submit

--- cypress/e2e/favorite.cy.ts
+++ cypress/e2e/favorite.cy.ts
@@ -1,16 +1,36 @@
 
+
 describe('favorite page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/favorites');
+    cy.intercept('GET', 'https://api.quotable.io/random', {
+      fixture: 'random.json'})
+    cy.intercept('GET', 'https://api.quotable.io/quotes?page=1', {
+      fixture: 'pageOne.json'})
+    cy.visit('http://localhost:3000')
   });
 
   it('Should render favorite page', () => {
+    cy.get('[class*=favorites]').click()
+    cy.url('http://localhost:3000/favorites')
     cy.contains('h1', 'zenton')
     cy.contains('h2', 'Favorites')
   })
 
+  it('Should be able to click heart to favorite quote and view that quote on favorite page', () => {
+    cy.get('[class*=favorite-button]').first().click()
+    cy.get('[class*=favorite-button]').last().click()
+    cy.get('[class*=favorites]').click()
+    cy.url('http://localhost:3000/favorites')
+    cy.contains('div', 'testing testing testing 1 2 3')
+    cy.contains('div', "Just be patient. Let the game come to you. Don't rush. Be quick, but don't hurry")
+    cy.get('[class*=user-quote]').should('have.length', 2)
+  })
+
   it('Should be able to return to home page', () => {
+    cy.get('[class*=favorites]').click()
+    cy.url('http://localhost:3000/favorites')
     cy.get('[class*=home]').click()
+    cy.url('http://localhost:3000')
     cy.contains('h1', 'zenton')
     cy.contains('h2', 'Home')
   })
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,20 +1,12 @@
 
+
 describe('home page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
-
     cy.intercept('GET', 'https://api.quotable.io/random', {
-      "_id": "CPhGOJeapNYZ",
-      "content": "testing testing testing 1 2 3",
-      "author": "Sister beretta",
-      "tags": [
-        "famous-quotes"
-      ],
-      "authorSlug": "sister-beretta",
-      "length": 24,
-      "dateAdded": "2020-09-09",
-      "dateModified": "2020-09-09"
-    })
+      fixture: 'random.json'})
+    cy.intercept('GET', 'https://api.quotable.io/quotes?page=1', {
+      fixture: 'pageOne.json'})
+    cy.visit('http://localhost:3000')
   });
 
   it('Should render home page', () => {
@@ -22,17 +14,25 @@ describe('home page', () => {
     cy.contains('h2', 'Home')
   })
 
+  it('Should render correct amount of quotes on page load (a random quote and page of quotes from api)', () => {
+    cy.get('[class*=user-quote]').should('have.length', 21)
+    cy.contains('div', 'testing testing testing 1 2 3')
+    cy.contains('div', "I've missed more than 90000000000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.")
+  })
+
   it('Should be able to write quote', () => {
     cy.get('textarea[name="quote"]')
       .type('you only live once')
     cy.get('[class*=quote-button]').click()
     cy.contains('div', 'you only live once')
+    cy.get('[class*=user-quote]').should('have.length', 22)
   })
 
   it('Should be able to click heart to favorite quote', () => {
     cy.get('[class*=favorite-button]').first().click()
     cy.get('[class*=favorites]').click()
     cy.contains('div', 'testing testing testing 1 2 3')
+    cy.contains('div', 'Sister beretta')
   })
 
   it('Should render a error message if user visits invalid URL', () => {
